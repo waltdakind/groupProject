88d7634926bf74976e0e88c1663a501b99832d6e
Steven Graboski <sgrabosk@gmail.com>
2016-03-16 19:01:48 -0400

    Initial Commit

---
+++ assets/js/tweets.js
@@ -0,0 +1,117 @@
+/* 1: Global Vars
+ * ============== */
+
+// Trend Array
+var trends = [];
+
+// Firebase ref
+trendRef = new Firebase('https://uncommitteds.firebaseio.com/');
+
+/* 2: Functions / Obejects
+ * ======================= */
+
+// main interface
+var interface = {
+
+	formatter : function(trendName){
+		// first, remove any initial hashtags
+		var newName = "";
+		if (trendName[0] === '#') {
+			trendName = trendName.slice(1);
+		}
+
+		// now find every instance of a capital letter, starting with second char
+		for (var i = 1; i < trendName.length; i++) {
+
+			// save current and next char's ascii
+			var curChar = trendName.charCodeAt(i);
+			var nextChar = trendName.charCodeAt(i+1);
+			var lastChar = trendName.charCodeAt(i-1);
+
+			// if curChar is the value for a cap letter, add a space before it...
+			if ( 65 <= curChar && curChar <= 95 ) {
+				// ...but only if there's no issue with other chars
+				if (trendName[i-1] !== " " && trendName[i-1] !== "-") {
+					if (65 >= lastChar || lastChar >= 95) {
+							trendName = trendName.slice(0, i) + " " + trendName.slice(i);
+							// increase counter by 1 on success
+							i++;
+							console.log(trendName);
+					}
+				}
+			}
+		}
+
+		// return the new word
+		return trendName;
+	},
+
+	// api method: call twitter API for Trends, save it to firebase
+	api: function() {
+		// grab a snapshot of the database this one time
+		trendRef.once("value", function(snapshot){
+			// catch the timestamp of the last update
+			if (snapshot.child("twitter").child("timeAdded").exists()) {
+				var time_record = snapshot.child("twitter").val().timeAdded;
+				console.log(time_record);
+				// catch the current timestamp
+				var time_now = Date.now();
+				console.log(time_now);
+				// figure out the dif between the recorded time and now
+				var time_dif = time_now - time_record;
+				console.log(time_dif);
+			}
+			if (time_dif >= 300000 || !snapshot.child("twitter").child("timeAdded").exists()) {
+
+				// then make ajax call
+				$.ajax({
+
+					// call our get_tweets php file
+					url: 'get_tweets.php',
+					type: 'GET',
+
+					// on success, save all latest trends to the global trend arr
+					success: function(response) {
+
+						// first remove all data from firebase
+						trendRef.child('twitter').set({
+						
+						})
+
+						// shortcut for trends in api
+						var latestTrends = response[0].trends;
+
+						// go through each trend
+						for (var i = 0; i < latestTrends.length; i++){
+
+							// format the name of the current trend
+							latestTrends[i].name = interface.formatter(latestTrends[i].name);
+
+							// push all of the current trend's data to firebase
+							trends.push(latestTrends[i]);
+						}
+
+						// send our trends arr to firebase, along with a timestamp
+						trendRef.child("twitter").set({
+							trends : trends,
+	          	timeAdded: Firebase.ServerValue.TIMESTAMP
+						})
+					},
+					// send error message on error
+					error: function(errors) {
+						console.log(errors);
+					}
+				})
+			}
+			// if updated within 5 mins, tell us how many mins we have left for new data
+			else {
+				var minutes = Math.ceil(5 - (time_dif / 60000));
+				console.log("already updated. Wait " + minutes + " mins for a new update");
+			}
+		})
+	},
+};
+
+
+// 3: Calls
+$(document).on("load", interface.api());
---
+++ get_tweets.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('twitter_proxy.php');
+
+// Twitter OAuth Config options
+$oauth_access_token = '309792138-MCuIbvttkOhZIJl0VwGY31SvfmvB70ljyHW4gJ3d';
+$oauth_access_token_secret = 'yXQQZLRLFFH4tcANRiXD0wcrlSAV4tJB9obtFaet0VXKi';
+$consumer_key = '3XDKFbqEJtM2m03myki998wTH';
+$consumer_secret = 'CBgxlKx08piTzW3D1aThWlVE0agoXyLDtIOk2F94stYkOEj8zC';
+$woe_id = 23424977;
+
+$twitter_url = 'trends/place.json';
+$twitter_url .= '?id=' . $woe_id;
+// $twitter_url .= '&screen_name=' . $screen_name;
+// $twitter_url .= '&count=' . $count;
+
+// Create a Twitter Proxy object from our twitter_proxy.php class
+$twitter_proxy = new TwitterProxy(
+	$oauth_access_token,			// 'Access token' on https://apps.twitter.com
+	$oauth_access_token_secret,		// 'Access token secret' on https://apps.twitter.com
+	$consumer_key,					// 'API key' on https://apps.twitter.com
+	$consumer_secret,				// 'API secret' on https://apps.twitter.com
+	$user_id,						// User id (http://gettwitterid.com/)
+	$woe_id,					// Location (1 = world)
+	$count							// The number of tweets to pull out
+);
+
+// Invoke the get method to retrieve results via a cURL request
+$tweets = $twitter_proxy->get($twitter_url);
+
+echo $tweets;
+
+?>
